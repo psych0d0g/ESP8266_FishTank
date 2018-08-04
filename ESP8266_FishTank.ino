5f841049ac2fba463b7d97200770cfd0e1c58ea5
Lukas Wingerberg <bigluks@gmail.com>
2018-08-04 20:02:38 +0200

    Add mqtt functionality

--- ESP8266_FishTank.ino
+++ ESP8266_FishTank.ino
@@ -3,42 +3,15 @@
 #include <FS.h>
 #include <Wire.h>
 #include <DNSServer.h>
-#include <ESP8266WiFi.h>
 #include <WiFiManager.h>
 #include <ArduinoJson.h>
-#include <HardwareSerial.h>
-//#include <MQTT.h>
 
 #include "lib/user_config.h"
 #include "lib/display.h"
 #include "lib/sensors.h"
-#include "lib/webserver_setup.h"
+#include "lib/webserver.h"
+#include "lib/mqtt_handler.h"
 
-//WiFiClient net;
-//MQTTClient client; 
-
-/**
-void messageReceived(String &topic, String &payload) {
-  Serial.println("incoming: " + topic + " - " + payload);
-}
-
-void connectMqtt() {
-  while (!client.connect("arduino", "try", "try")) {
-    Serial.print(".");
-    delay(1000);
-  }
-
-  client.subscribe("/fishtank/command/maxtemp");
-  client.subscribe("/fishtank/command/maxdiff");
-
-  Serial.print ( "MQTT Connected to: " + String(mqtt_server) );
-}
-
-void publishMessage(String topic, String payload) {
-  client.publish(topic, payload);
-  Serial.println("Published: " + topic + " - " + payload);
-}
-*/
 /**
  * setup
  *
@@ -60,32 +33,14 @@ void setup()
 
   Serial.println ( "IP address: " + String(WiFi.localIP()) );
 
- // client.begin(mqtt_server, net);
- // client.onMessage(messageReceived);
-  mlx.begin();  
-  //connectMqtt();
+  setupSensors();
   readConfig();
   displaySetup();
+  webserverSetup();
+  setupMqtt();
   /*return index page which is stored in serverIndex */
-  server.on ( "/", rootHandler );
-  server.on ( "/settings", SettingsHandler );
-  server.on ( "/fishtank.json", apiHandler );
-  server.begin();
-  Serial.println ( "HTTP server started" );
-
-
-
   pinMode(FAN_PIN, OUTPUT);
-  pinMode(TRIG_PIN, OUTPUT);
-  pinMode(ECHO_PIN, INPUT);
-  tempDiff = tempInt - config.desired_temp;
-  if(tempDiff < 0) {
-    tempDiff = 0;
-  }
 
-  //turn the PID on
-  myPID.SetMode(AUTOMATIC);
-  myPID.SetOutputLimits(commandMin, commandMax);
 }
 
 /**
@@ -98,7 +53,6 @@ void loop()
   handleTempLoop();
   handleWaterLevel();
   handleDisplay();
-//  client.subscribe("/fishtank/command/maxtemp");
-//  client.subscribe("/fishtank/command/maxdiff");
+  handleMQTT();
   server.handleClient();
 }
